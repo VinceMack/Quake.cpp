92d790dc379c33c07b44657582c8bb3df8d1b32f
VinceMack <sentinelvhast@gmail.com>
2026-07-16 08:13:47 -0700

    Refactor chase.cpp

--- src/chase.cpp
+++ src/chase.cpp
@@ -1,5 +1,6 @@
 // chase.cpp -- chase camera code
-
+#include <cmath>
+#include <numbers>
 #include "quakedef.hpp"
 
 using namespace Client;
@@ -25,17 +26,28 @@ using namespace Wad;
 using namespace Cvar;
 using namespace Cmd;
 
-
 namespace Client {
+
+namespace {
 
 cvar_t chase_back = { "chase_back", "100", {}, {}, {}, {} };
 cvar_t chase_up = { "chase_up", "16", {}, {}, {}, {} };
 cvar_t chase_right = { "chase_right", "0", {}, {}, {}, {} };
-cvar_t chase_active = { "chase_active", "0", {}, {}, {}, {} };
 
 Vector3 chase_dest;
 
-void Chase_Init(void)
+void TraceLine(const Vector3& start, const Vector3& end, Vector3& impact)
+{
+    trace_t trace{};
+    SV_RecursiveHullCheck(cl.worldmodel->hulls, 0, 0, 1, start, end, &trace);
+    impact = trace.endpos;
+}
+
+} // namespace
+
+cvar_t chase_active = { "chase_active", "0", {}, {}, {}, {} };
+
+void Chase_Init()
 {
     Cvar::Register(&chase_back);
     Cvar::Register(&chase_up);
@@ -43,23 +55,12 @@ void Chase_Init(void)
     Cvar::Register(&chase_active);
 }
 
-void TraceLine(const Vector3& start, const Vector3& end, Vector3& impact)
+void Chase_Update()
 {
-    trace_t trace;
-
-    trace = {};
-    SV_RecursiveHullCheck(cl.worldmodel->hulls, 0, 0, 1, start, end, &trace);
-
-    impact = trace.endpos;
-}
-
-void Chase_Update(void)
-{
-    float dist;
-    Vector3 forward, up, right;
-    Vector3 dest, stop;
-
     // if can't see player, reset
+    Vector3 forward;
+    Vector3 up;
+    Vector3 right;
     AngleVectors(cl.viewangles, forward, right, up);
 
     // calc exact destination
@@ -67,17 +68,18 @@ void Chase_Update(void)
     chase_dest.z = r_refdef.vieworg.z + chase_up.value;
 
     // find the spot the player is looking at
-    dest = r_refdef.vieworg + forward * 4096.0f;
+    Vector3 dest = r_refdef.vieworg + forward * 4096.0f;
+    Vector3 stop;
     TraceLine(r_refdef.vieworg, dest, stop);
 
     // calculate pitch to look at the same spot from camera
     stop = stop - r_refdef.vieworg;
-    dist = stop.dot(forward);
-    if (dist < 1) {
-        dist = 1;
+    float dist = stop.dot(forward);
+    if (dist < 1.0f) {
+        dist = 1.0f;
     }
 
-    r_refdef.viewangles[PITCH] = static_cast<float>(-atan(stop.z / dist) / M_PI * 180);
+    r_refdef.viewangles[PITCH] = static_cast<float>(-std::atan(stop.z / dist) / std::numbers::pi * 180.0f);
 
     // move towards destination
     r_refdef.vieworg = chase_dest;
